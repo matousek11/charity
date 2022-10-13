6f33d12b195ab68ddbdec1f4fa8b555b87aeb792
Martin Trnka <trnka.martin@icloud.com>
2022-10-13 12:28:53 +0200

    Přidání Item.js edit index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Item from '../src/modules/common/Item'
 import Table from '../src/modules/common/Table'
 import styles from '../styles/Home.module.css'
 
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <div>
         <Table />
+        <Item />
       </div>
     </>
   )
