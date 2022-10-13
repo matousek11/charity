f38d33c109a48c12ce21242be30c94d4a756bfea
lukas matousek <matousek1199@seznam.cz>
2022-10-13 11:35:37 +0200

    Přidání mapy

---
+++ pages/map.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import Head from 'next/head'
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+}
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+}
+
+const map = () => {
+  return (
+    <div>
+      <LoadScript googleMapsApiKey="AIzaSyByft-k4seLg0milNzy2vE4oSEjhRmjjAY">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  )
+}
+
+export default map
