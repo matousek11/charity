e6b71ce2681800967ad0f978d721622272bc69ce
Martin Trnka <trnka.martin@icloud.com>
2022-10-13 10:51:11 +0200

    Přidaný vzhled navbaru - nikam nepřesměrovává a je tam button navíc

--- src/modules/common/Layout.js
+++ src/modules/common/Layout.js
@@ -5,8 +5,47 @@ const Layout = ({ children }) => {
   return (
     <>
       <div>
-        <h1>Charita</h1>
-        <Button />
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">
+            <img src="/img/logo.svg" width="30" height="30" alt="" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Domů <span class="sr-only"></span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Všechny charity
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Filtr charit
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  O projektu
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Button text="Jméno komponenty" />
       </div>
       {children}
     </>
